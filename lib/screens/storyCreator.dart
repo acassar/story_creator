8c81066bc56affee7405288ee2651edbd6edb8e5
Adrien Cassar <adricass@live.fr>
2023-08-31 16:21:39 +0200

    node creation

--- lib/screens/storyCreator.dart
+++ lib/screens/storyCreator.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:story_creator/components/storyNode.dart';
+import 'package:story_creator/models/edge.dart';
 import 'package:story_creator/models/story.dart';
 import 'package:story_creator/models/storyItem.dart';
 import 'package:story_creator/services/storyService.dart';
-
+import 'package:uuid/uuid.dart';
 
 import 'package:graphview/GraphView.dart';
-
 
 class StoryCreator extends StatefulWidget {
   const StoryCreator({super.key});
@@ -19,9 +19,12 @@ class _StoryCreatorState extends State<StoryCreator> {
   StoryService storyService = StoryService();
   Story? example;
   final Graph graph = Graph()..isTree = true;
-  BuchheimWalkerConfiguration builder = BuchheimWalkerConfiguration();
+  SugiyamaConfiguration builder = SugiyamaConfiguration();
+  StoryItem? selectedNode;
+  TextEditingController textController = TextEditingController();
+  TextEditingController choiceTextController = TextEditingController();
 
-@override
+  @override
   void initState() {
     super.initState();
     loadStory();
@@ -41,33 +44,112 @@ class _StoryCreatorState extends State<StoryCreator> {
     return example!.items.firstWhere((element) => element.id == id);
   }
 
+  nodeClickCallback(StoryItem item) {
+    setState(() {
+      selectedNode = item;
+    });
+  }
+
+  createNode() {
+    String id = Uuid().v4();
+    setState(() {
+      example!.items.add(StoryItem(id, textController.text, choiceText: choiceTextController.text));
+      example!.edges.add(StoryEdge(selectedNode!.id, id));
+      graph.addEdge(Node.Id(selectedNode!.id), Node.Id(id));
+      selectedNode = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if(example == null) return const Placeholder();
-    return Column(
-      children: [
-        Expanded(
-              child: InteractiveViewer(
-                  constrained: false,
-                  boundaryMargin: const EdgeInsets.all(100),
-                  minScale: 0.01,
-                  maxScale: 5.6,
-                  child: GraphView(
-                    graph: graph,
-                    algorithm: BuchheimWalkerAlgorithm(builder, TreeEdgeRenderer(builder)),
-                    paint: Paint()
-                      ..color = Colors.green
-                      ..strokeWidth = 1
-                      ..style = PaintingStyle.stroke,
-                    builder: (Node node) {
-                      // I can decide what widget should be shown here based on the id
-                      var a = node.key!.value;
-                      return StoryNode(item: findNode(a),);
-                    },
-                  )),
+    if (example == null) return const Placeholder();
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            // height: 100,
+            decoration: const BoxDecoration(color: Colors.black12),
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Text("choice to: ${selectedNode?.id}"),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        const Text("text"),
+                        Container(
+                          width: 400,
+                          margin: const EdgeInsets.all(5),
+                          color: Colors.black26,
+                          child: TextField(
+                            controller: textController,
+                            maxLines: 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("choice text"),
+                        Container(
+                          width: 400,
+                          margin: const EdgeInsets.all(5),
+                          color: Colors.black26,
+                          child: TextField(
+                            controller: choiceTextController,
+                            maxLines: 3,
+                          ),
+                        ),
+                      ],
+                    ),
+                    MaterialButton(
+                         onPressed: selectedNode != null ? createNode : null,
+                          child: Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: const BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: const Text("submit")
+                          ),
+                        )
+                  ],
+                )
+              ],
             ),
-      ],
+          ),
+          Flexible(
+            child: InteractiveViewer(
+                constrained: false,
+                boundaryMargin: const EdgeInsets.all(double.infinity),
+                minScale: 0.01,
+                maxScale: 5.6,
+                child: Column(
+                  children: [
+                    GraphView(
+                      graph: graph,
+                      algorithm: SugiyamaAlgorithm(
+                          builder),
+                      paint: Paint()
+                        ..color = Colors.green
+                        ..strokeWidth = 1
+                        ..style = PaintingStyle.stroke,
+                      builder: (Node node) {
+                        // I can decide what widget should be shown here based on the id
+                        var id = node.key!.value;
+                        return StoryNode(
+                          item: findNode(id),
+                          callack: nodeClickCallback,
+                          key: Key(id),
+                        );
+                      },
+                    ),
+                  ],
+                )),
+          ),
+        ],
+      ),
     );
   }
 }
